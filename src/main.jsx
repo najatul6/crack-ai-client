5c991f8d33689811ec32c8ab94d888983262c161
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-10-04 23:23:16 +0600

    install tanstack react-query

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 
+const client = new QueryClient();
 createRoot(document.getElementById('root')).render(
   <StrictMode>
    <RouterProvider router={router} />
