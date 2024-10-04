0d3c0b18099ad63de887e514cb38f77b616c5395
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-10-04 23:27:57 +0600

    modify main route

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { RouterProvider } from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import MainRoutes from "./Routers/mainRoutes";
+import "animate.css";
+import AuthProvider from "./providers/AuthProvider";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const client = new QueryClient();
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-   <RouterProvider router={router} />
-  </StrictMode>
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <QueryClientProvider client={client}>
+      <AuthProvider>
+        <RouterProvider router={MainRoutes}></RouterProvider>
+      </AuthProvider>
+    </QueryClientProvider>
+  </React.StrictMode>
+);
