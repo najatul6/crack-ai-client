ca59adc025712b6ba07bfede780ff813a1861fca
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-10-05 21:27:42 +0600

    update title component

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,36 +1,10 @@
-const Social = () => {
+const Title = ({ children }) => {
     return (
-      <div className=" bg-white shadow py-3 rounded-full flex flex-col items-center">
-        <div>
-          <img
-            className="w-[64px]"
-            src="https://img.icons8.com/?size=96&id=17949&format=png"
-            alt=""
-          />
-        </div>
-        <div className="">
-          <img
-            className="w-[64px]"
-            src="https://img.icons8.com/?size=96&id=118497&format=png"
-            alt=""
-          />
-        </div>
-        <div className="">
-          <img
-            className="w-[64px]"
-            src="https://img.icons8.com/?size=96&id=bUGbDbW2XLqs&format=png"
-            alt=""
-          />
-        </div>
-        <div className="">
-          <img
-            className="w-[64px]"
-            src="https://img.icons8.com/?size=128&id=3tC9EQumUAuq&format=png"
-            alt=""
-          />
-        </div>
+      <div className="relative border-s-8 border-orange-400 ps-3">
+        <h2 className="text-6xl font-bold">{children}</h2>
+        <p className="absolute bottom-0 text-9xl -z-10 opacity-5">{children}</p>
       </div>
     );
   };
   
-  export default Social;
+  export default Title;
