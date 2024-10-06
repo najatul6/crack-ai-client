5fc97b0bcab94903c94cf9aed4b1fa178b3dc018
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-10-06 19:53:45 +0600

    update api keys

--- src/pages/GeneratePainting.jsx
+++ src/pages/GeneratePainting.jsx
@@ -1,22 +1,41 @@
 import Title from "../components/Title";
 
 const GeneratePainting = () => {
-    const handleSubmit=(e)=>{
-        e.preventDefault()
-        console.log(e.target.prompt.value);
-    }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target.prompt.value);
+    const form = new FormData();
+    form.append("prompt", e.target.prompt.value);
+
+    fetch("https://clipdrop-api.co/text-to-image/v1", {
+      method: "POST",
+      headers: {
+        "x-api-key": YOUR_API_KEY,
+      },
+      body: form,
+    })
+    //   .then((response) => response.arrayBuffer())
+    //   .then((buffer) => {
+    //     // buffer here is a binary representation of the returned image
+    //   });
+  };
   return (
     <div className="container">
       <Title>GeneratePainting</Title>
 
-      <form onSubmit={handleSubmit} className="flex flex-wrap gap-3 mt-10 justify-center w-full">
+      <form
+        onSubmit={handleSubmit}
+        className="flex flex-wrap gap-3 mt-10 justify-center w-full"
+      >
         <textarea
           type="text"
           name="prompt"
           placeholder="What kind of painting you want ..."
           className="textarea textarea-bordered w-10/12 py-4"
         />
-        <button type="submit" className="w-10/12 rounded-xl">Generate</button>
+        <button type="submit" className="w-10/12 rounded-xl">
+          Generate
+        </button>
       </form>
     </div>
   );
