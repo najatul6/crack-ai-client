4ff3a9d16486cf0fdf0ec85960e50fef553e9ea0
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-10-08 20:35:50 +0600

    update route problem

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
-import MainRoutes from "./Routers/mainRoutes";
+import MainRoutes from "./Routers/MainRoutes";
 import "animate.css";
 import AuthProvider from "./providers/AuthProvider";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
