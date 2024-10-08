4c474c82f410672d1718dbf2d4c024a0ec1cfe0f
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-10-08 19:35:33 +0600

    add Partnar component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import FrequentlyQuestion from "../components/FrequentlyQuestion";
 import Jumbtorn from "../components/Jumbtorn";
+import Partnar from "../components/Partnar";
 
 const Home = () => {
   return (
     <>
     <FrequentlyQuestion/>
     <Jumbtorn/>
+    <Partnar/>
     </>
   );
 };
