6e0a0066d297dc0312bc52f2abfeade62138dd68
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-10-08 18:37:25 +0600

    update painting details and from

--- src/pages/PaintingDetails.jsx
+++ src/pages/PaintingDetails.jsx
@@ -2,14 +2,17 @@ import { useLoaderData } from "react-router-dom";
 
 const PaintingDetails = () => {
   const data = useLoaderData();
+  console.log(data);
   return (
     <div className="container">
       <div className="grid md:grid-cols-2 gap">
         <div>
-          <img src={data?.imageUrl} alt="" className="w-full" />
+          <img src={data?.imageUrl} alt={data?.title} className="w-full" />
         </div>
         <div className="space-y-5">
           <h2 className="text-2xl">{data?.title}</h2>
+          <p className="p-3 border-black border-2 rounded-full  transition-all duration-200 backdrop-blur-lg shadow-inner shadow-black">{data?.category}</p>
+          <p className="p-3 border-black border-2 rounded-full  transition-all duration-200 backdrop-blur-lg shadow-inner shadow-black">{data?.type}</p>
           <p>{data?.detail}</p>
         </div>
       </div>
